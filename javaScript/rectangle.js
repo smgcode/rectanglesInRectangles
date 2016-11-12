c16e07cca7ef10a090fe23ba7921e3970bc42091
Sean Gee <seangee@gmail.com>
2016-11-12 13:06:36 -0800

    DRY up listeners, update form

--- javaScript/rectangle.js
+++ javaScript/rectangle.js
@@ -7,47 +7,49 @@ const TENTHS = 10
 
 module.exports = function () {
   console.log("rectangle.js")
-  let $dimXmmVal = $(".js-x-mm")
-  let $dimXinVal = $(".js-x-in-val")
-  let $dimXin = $(".js-x-in")
-  let $dimYmmVal = $(".js-y-mm")
-  let $dimYinVal = $(".js-y-in-val")
-  let $dimYin = $(".js-y-in")
 
-  $dimXin.addClass("grey")
-  $dimYin.addClass("grey")
+  onChangeDimension = function($input, $output, conversion, precision){
+    $output.addClass("orange")
+    $input.on("input", function(){
+      let value = this.value
+      let $outputVal = $output.find(".js-value")
+      if(value != undefined && value.length && value > 0){
+        $output.addClass("teal")
+        $output.removeClass("orange")
+        value *= conversion
+      }
+      else{
+        $output.removeClass("teal")
+        $output.addClass("orange")
+        value = 0
+      }
+      $outputVal.html(Math.round(value * precision) / precision)
+    })
+  }
 
-  $dimXmmVal.on("input", function(){
-    let valXmm = this.value
-    let valXin = 0
-    if(valXmm != undefined && valXmm.length && valXmm > 0){
-      $dimXin.addClass("teal")
-      $dimXin.removeClass("grey")
-      valXin = valXmm * MM_TO_INCHES
-    }
-    else{
-      $dimXin.removeClass("teal")
-      $dimXin.addClass("grey")
-      valXin = 0
-    }
-    valX = Math.round(valXin * THOUSANDTHS) / THOUSANDTHS
-    $dimXinVal.html(valX)
-  })
+  $('.js-form-small')
+    .form({
+      on: 'blur',
+      fields: {
+        integer: {
+          identifier  : 'integer',
+          rules: [
+            {
+              type   : 'integer[1..]',
+              prompt : 'Please enter an integer value'
+            }
+          ]
+        }
+      }
+    })
+  ;
 
-  $dimYmmVal.on("input", function(){
-    let valYmm = this.value
-    let valYin = 0
-    if(valYmm != undefined && valYmm.length && valYmm > 0){
-      $dimYin.addClass("teal")
-      $dimYin.removeClass("grey")
-      valYin = valYmm * MM_TO_INCHES
-    }
-    else{
-      $dimYin.removeClass("teal")
-      $dimYin.addClass("grey")
-      valYin = 0
-    }
-    valY = Math.round(valYin * THOUSANDTHS) / THOUSANDTHS
-    $dimYinVal.html(valY)
-  })
+
+  // Small Rectangles
+  onChangeDimension($(".js-input-small-x"), $(".js-output-small-x"), MM_TO_INCHES, THOUSANDTHS)
+  onChangeDimension($(".js-input-small-y"), $(".js-output-small-y"), MM_TO_INCHES, THOUSANDTHS)
+
+  // Large Rectangle
+  onChangeDimension($(".js-input-large-x"), $(".js-output-large-x"), INCHES_TO_MM, TENTHS)
+  onChangeDimension($(".js-input-large-y"), $(".js-output-large-y"), INCHES_TO_MM, TENTHS)
 }
